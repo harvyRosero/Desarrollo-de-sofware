16f1107d504ee23011b5f5c2682226fbc3219771
harvyRosero <santiagorosero888@gmail.com>
2021-01-29 12:11:13 -0500

    sprint2

---
+++ Sprint 2 mintic/Reto 2/src/components/TeamCard.vue
@@ -0,0 +1,31 @@
+<template>
+        
+        
+            <div class="card text-white bg-dark">
+                <div class="d-flex justify-content-center p-2">
+                    <img v-bind:src = "member.imagen" alt="img-team" >
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title"> {{ member.nombre }} </h5>
+                    <p class="card-text"> {{ member.descripcion }} </p>
+                    <p class="card-text"> {{ member.rol }} </p>
+
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                
+            </div>
+        </div>
+
+</template>
+
+<script>
+    export default {
+        name: "TeamCard",
+        props: ['member']
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ Sprint 2 mintic/Reto 2/src/components/TopBanner.vue
@@ -0,0 +1,55 @@
+<template>
+<div>
+
+<top-menu></top-menu>
+
+<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="1" class=""></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="2" class=""></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="w-100" height="400px" src="../../public/img/carousel/skyscrapers-5838029_1280.jpg" alt="img-carousel">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="w-100" height="400px" src="../../public/img/carousel/fire-4892711_1920.jpg" alt="img-carousel">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Second slide label</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="w-100" height="400px" src="../../public/img/carousel/guanajuato-5838206_1920.jpg" alt="img-carousel">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Third slide label</h5>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
+</template>
+
+<script>
+import TopMenu from '@/components/TopMenu' 
+export default {
+    name: "TopBanner",
+    components:{
+        TopMenu
+    }
+}
+</script>
