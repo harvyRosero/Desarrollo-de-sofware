3fb1996f921a4277287b15bc9eac5409f1515379
harvyRosero <santiagorosero888@gmail.com>
2021-02-05 18:28:04 -0500

    front

---
+++ front-end/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <the-home>
+    </the-home>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TheHome from '@/components/auth/TheHome.vue'
+export default{
+  components:{
+    TheHome
+  }
+}
+</script>
---
+++ front-end/src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div >
+        <TheLogin>
+        </TheLogin>
+    </div>
+</template>
+
+<script>
+import TheLogin from '@/components/auth/TheLogin.vue'
+
+export default{
+    components:{
+        TheLogin
+    }
+}
+</script>
